ead03a5cb716ee4fff9385309f52f287193f9102
707Seven707 <sevenleniwiec@gmail.com>
2024-05-15 18:45:09 +0200

    Initial Commit for this project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public Main(){
+        setTitle("Plansza");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
