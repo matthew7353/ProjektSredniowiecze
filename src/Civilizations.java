0d428e40679eeeeba544daa3b35c2da0912981e1
Mateusz <matthew7353@gmail.com>
2024-05-16 12:10:48 +0200

    Added BoardList, that is keeping all the positions

--- src/Civilizations.java
+++ src/Civilizations.java
@@ -4,10 +4,16 @@ public class Civilizations {
     int startingPoint;
     Color color;
     Board board;
-    Civilizations(int startingPoint, int r, int g, int b, Board board){
+
+    int [] boardArray;
+    int id;
+    Civilizations(int startingPoint, int r, int g, int b, Board board, int [] boardArray, int id){
         this.startingPoint = startingPoint;
         this.color = new Color(r, g, b);
         this.board = board;
+        this.boardArray = boardArray;
+        this.id= id;
+        boardArray[startingPoint] = id;
         board.updateCell(startingPoint / 10, startingPoint % 10, color);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -17,10 +17,11 @@ public class Main extends JFrame {
     public static void main(String[] args) {
         Board board = new Board();
         ArrayList<Civilizations> array = new ArrayList<>();
-        array.add(new Civilizations(0, 0, 0, 255, board));
-        array.add(new Civilizations(9, 0, 255, 0, board));
-        array.add(new Civilizations(90, 255, 0, 0, board));
-        array.add(new Civilizations(99, 200, 50, 50, board));
+        int [] boardList = new int[100];
+        array.add(new Civilizations(69, 0, 0, 255, board, boardList, 1));
+        array.add(new Civilizations(9, 0, 255, 0, board, boardList, 2));
+        array.add(new Civilizations(90, 255, 0, 0, board, boardList, 3));
+        array.add(new Civilizations(99, 200, 50, 50, board, boardList, 4));
     }
     //hehe
 }
