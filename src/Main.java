f650809278f03870b33459f22046f4b48a269ce8
707Seven707 <sevenleniwiec@gmail.com>
2024-05-16 19:26:26 +0200

    Random and different location of Civilizations

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,43 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main extends JFrame {
-    /*
-    public Main(){
-        setTitle("Plansza");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(300, 300);
-        setLocationRelativeTo(null);
-        setVisible(true);
-    }
-    przeniesione do Board
-    */
+
+
+
 
     public static void main(String[] args) {
         Board board = new Board();
         ArrayList<Civilizations> array = new ArrayList<>();
         int [] boardList = new int[100];
-        array.add(new Civilizations(69, 0, 0, 255, board, boardList, 1));
-        array.add(new Civilizations(9, 0, 255, 0, board, boardList, 2));
-        array.add(new Civilizations(90, 255, 0, 0, board, boardList, 3));
-        array.add(new Civilizations(99, 200, 50, 50, board, boardList, 4));
+
+        //Losowanie koorydnatów
+        int w1 = 0;
+        ArrayList<Integer> pozycje = new ArrayList<>();
+        while (w1<4) {
+            Random rand = new Random();
+            int n = rand.nextInt(100);
+            int jest = 0;
+
+            for(Integer x : pozycje){
+                if(n == x){
+                    jest++;
+                }
+            }
+            if(jest==0){
+                pozycje.add(n);
+                w1++;
+            }
+
+        }
+
+
+        array.add(new Civilizations(pozycje.get(0), 0, 0, 255, board, boardList, 1));
+        array.add(new Civilizations(pozycje.get(1), 0, 255, 0, board, boardList, 2));
+        array.add(new Civilizations(pozycje.get(2), 255, 0, 0, board, boardList, 3));
+        array.add(new Civilizations(pozycje.get(3), 200, 50, 50, board, boardList, 4));
     }
-    //hehe
+
 }
